d598879b498ad671d7339fc6a7fbe4f57d188a2b
Nyandika Donald <donald.nyandika@gmail.com>
2023-03-21 18:23:12 +0000

    Set up testing

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import eslint from 'vite-plugin-eslint';
@@ -5,6 +6,15 @@ import eslint from 'vite-plugin-eslint';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), eslint()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './test-setup.ts',
+    coverage: {
+      provider: 'istanbul',
+      reporter: ['text', 'json', 'html'],
+    },
+  },
   server: {
     watch: {
       usePolling: true,
