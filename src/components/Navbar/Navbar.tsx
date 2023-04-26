90e0fde3a2d6f12360673f8e91d4676fed87bd53
Rioba-Ian <riobafelix@gmail.com>
2023-04-26 13:36:11 +0300

    design navbar in desktop, style cta-links

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -3,31 +3,37 @@ import SpaceyaTechHome from '../../assets/SpaceyaTechHomeLogo.svg';
 import './Navbar.css';
 function Navbar() {
   return (
-    <nav className="navbar">
-      <div className="navbar-logo">
-        <img src={SpaceyaTechHome} alt="SpaceyaTech logo" />
-      </div>
-      <ul className="navbar-list">
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/forums">Forums</Link>
-        </li>
-        <li>
-          <Link to="/events">Events</Link>
-        </li>
-        <li>
-          <Link to="/blog">Blog</Link>
-        </li>
-        <li>
-          <Link to="/about">About Us</Link>
-        </li>
-      </ul>
+    <nav>
+      <div className="navbar">
+        <div className="navbar-logo">
+          <img src={SpaceyaTechHome} alt="SpaceyaTech logo" />
+        </div>
+        <ul className="navbar-list">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/forums">Forums</Link>
+          </li>
+          <li>
+            <Link to="/events">Events</Link>
+          </li>
+          <li>
+            <Link to="/blog">Blog</Link>
+          </li>
+          <li>
+            <Link to="/about">About us</Link>
+          </li>
+        </ul>
 
-      <div className="nav-cta-links">
-        <Link to="/login">Login</Link>
-        <Link to="/signup">Signup</Link>
+        <div className="cta-links">
+          <Link to="/login" className="login">
+            Login
+          </Link>
+          <Link to="/signup" className="signup">
+            Sign in
+          </Link>
+        </div>
       </div>
     </nav>
   );
