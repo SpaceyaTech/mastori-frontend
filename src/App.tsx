dc96ac4ba2f43fb581cfbbea6d917c66e08c257a
Rioba-Ian <riobafelix@gmail.com>
2023-04-26 12:57:24 +0300

    remove vite css styles and include css defaults

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import './App.css';
 import reactLogo from './assets/react.svg';
 import Navbar from './components/Navbar/Navbar';
 
@@ -21,17 +20,6 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   );
 }
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,9 +1,13 @@
 import { Link } from 'react-router-dom';
-
+import SpaceyaTechHome from '../../assets/SpaceyaTechHomeLogo.svg';
+import './Navbar.css';
 function Navbar() {
   return (
-    <nav>
-      <ul>
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <img src={SpaceyaTechHome} alt="SpaceyaTech logo" />
+      </div>
+      <ul className="navbar-list">
         <li>
           <Link to="/">Home</Link>
         </li>
@@ -19,13 +23,12 @@ function Navbar() {
         <li>
           <Link to="/about">About Us</Link>
         </li>
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-        <li>
-          <Link to="/signup">Signup</Link>
-        </li>
       </ul>
+
+      <div className="nav-cta-links">
+        <Link to="/login">Login</Link>
+        <Link to="/signup">Signup</Link>
+      </div>
     </nav>
   );
 }
